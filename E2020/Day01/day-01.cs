8c9039e0c93d6ba08abb2f79e4a7536878a7e30a
Dimitry Slynko <slynko@gmail.com>
2021-11-16 17:08:26 +0100

    Initial

---
+++ E2020/Day01/day-01.cs
@@ -0,0 +1,10 @@
+namespace AdventureOfCode.E2020
+{
+    public class Day01
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Day 01");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventureOfCode.E2020;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello AdventureOfCode!\n");
+
+            Day01.Run();
+        }
+    }
+}
