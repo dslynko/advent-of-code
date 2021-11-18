69b63296db7bb08e869faecceedb2564dec84395
Dimitry Slynko <slynko@gmail.com>
2021-11-18 17:18:19 +0100

    Day3, 2nd part

--- Python/E2020/Day03/day-03.py
+++ Python/E2020/Day03/day-03.py
@@ -1,8 +1,5 @@
 print("Day 03");
 
-dx = 3
-dy = 1
-
 class Point:
     def __init__(self, x = 0, y = 0):
         self.x = x
@@ -12,7 +9,7 @@ def ReadInput():
     with open("./input.txt") as input:
         return input.read().splitlines()
 
-def NextPoint(currentPoint: Point):
+def NextPoint(currentPoint: Point, dx, dy):
     nx = currentPoint.x + dx if currentPoint.x + dx < width else currentPoint.x + dx - width
 
     return Point(nx, currentPoint.y + dy)
@@ -22,21 +19,35 @@ area = ReadInput()
 height = len(area)
 width = len(area[0])
 
-print(f"{width}x{height}")
+def GoDownTheSlope(dx, dy):
+    point = Point(0, 0)
 
-point = Point(0, 0)
+    trees = 0
 
-trees = 0
+    while(point.y < height - 1):
+        point = NextPoint(point, dx, dy)
 
-while(point.y < height - 1):
-    point = NextPoint(point)
+        if(area[point.y][point.x] == "#"):
+            trees += 1
+            tree = "X"
+        else:
+            tree = "O"
 
-    if(area[point.y][point.x] == "#"):
-        trees += 1
-        tree = 'X'
-    else:
-        tree = 'O'
+        print(area[point.y][:point.x] + tree + area[point.y][point.x + 1:])
 
-    print(area[point.y][:point.x] + tree + area[point.y][point.x + 1:])
+    return trees
 
-print(f" Answer 1st part is {trees}")
+def Part1():
+    return GoDownTheSlope(3, 1)
+
+def Part2():
+    slope11 = GoDownTheSlope(1, 1)
+    slope31 = GoDownTheSlope(3, 1)
+    slope51 = GoDownTheSlope(5, 1)
+    slope71 = GoDownTheSlope(7, 1)
+    slope12 = GoDownTheSlope(1, 2)
+
+    return slope11 * slope31 * slope51 * slope71 * slope12
+
+print(f" Answer 1st part is {Part1()}")
+print(f" Answer 1st part is {Part2()}")
