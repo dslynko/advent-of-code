16dd030d5bbde4b59e6355da836c1bade98429b4
Dimitry Slynko <slynko@gmail.com>
2021-12-02 12:26:35 +0100

    Day 02 minor updates

--- Python/E2021/Day02/day-02.py
+++ Python/E2021/Day02/day-02.py
@@ -1,24 +1,30 @@
 print("E2021 - Day 02");
 
 class Maneuver:
-    def __init__(self, line):
-        action = line.split(" ")
+    def __init__(self, command):
+        action = command.split(" ")
 
         self.direction = action[0]
         self.distance = int(action[1])
 
-class U96:
+class IDasBoot:
+    def ExecuteManeuver(self, action: Maneuver):
+        pass
+    def GetPosition(self) -> int:
+        pass
+
+class U96(IDasBoot):
     def __init__(self):
         self.forward = 0
         self.depth = 0
 
     def ExecuteManeuver(self, action: Maneuver):
-            if(action.direction == "forward"):
-                self.forward += action.distance
-            else:
-                self.depth += (action.distance if action.direction == "down" else action.distance * -1)
+        if(action.direction == "forward"):
+            self.forward += action.distance
+        else:
+            self.depth += (action.distance if action.direction == "down" else action.distance * -1)
 
-    def Position(self):
+    def GetPosition(self):
         return self.forward * self.depth
 
 class U96Aimed(U96):
@@ -28,29 +34,18 @@ class U96Aimed(U96):
         self.aim = 0
 
     def ExecuteManeuver(self, action: Maneuver):
-            if(action.direction == "forward"):
-                self.forward += action.distance
-                self.depth += action.distance * self.aim
-            else:
-                self.aim += (action.distance if action.direction == "down" else action.distance * -1)
+        if(action.direction == "forward"):
+            self.forward += action.distance
+            self.depth += action.distance * self.aim
+        else:
+            self.aim += (action.distance if action.direction == "down" else action.distance * -1)
 
-def Part1():
-    dasBoot = U96()
-
+def PartX(dasBoot: IDasBoot):
     with open('./input.txt') as input:
         for line in input:
             dasBoot.ExecuteManeuver(Maneuver(line))
 
-    return dasBoot.Position()
+    return dasBoot.GetPosition()
 
-def Part2():
-    dasBoot = U96Aimed()
-
-    with open('./input.txt') as input:
-        for line in input:
-            dasBoot.ExecuteManeuver(Maneuver(line))
-
-    return dasBoot.Position()
-
-print(f" Answer 1st part is {Part1()}")
-print(f" Answer 2st part is {Part2()}")
+print(f" Answer 1st part is {PartX(U96())}")
+print(f" Answer 2st part is {PartX(U96Aimed())}")
