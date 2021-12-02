d36e6cae1f40212b8b4bee179ad71bf9fc577970
Dimitry Slynko <slynko@gmail.com>
2021-12-02 12:17:40 +0100

    Day 02 military style

--- Python/E2021/Day02/day-02.py
+++ Python/E2021/Day02/day-02.py
@@ -1,43 +1,56 @@
 print("E2021 - Day 02");
 
-class U96Action:
+class Maneuver:
     def __init__(self, line):
         action = line.split(" ")
 
         self.direction = action[0]
         self.distance = int(action[1])
 
+class U96:
+    def __init__(self):
+        self.forward = 0
+        self.depth = 0
+
+    def ExecuteManeuver(self, action: Maneuver):
+            if(action.direction == "forward"):
+                self.forward += action.distance
+            else:
+                self.depth += (action.distance if action.direction == "down" else action.distance * -1)
+
+    def Position(self):
+        return self.forward * self.depth
+
+class U96Aimed(U96):
+    def __init__(self):
+        U96.__init__(self)
+
+        self.aim = 0
+
+    def ExecuteManeuver(self, action: Maneuver):
+            if(action.direction == "forward"):
+                self.forward += action.distance
+                self.depth += action.distance * self.aim
+            else:
+                self.aim += (action.distance if action.direction == "down" else action.distance * -1)
+
 def Part1():
-    forward = 0
-    depth = 0
+    dasBoot = U96()
 
     with open('./input.txt') as input:
         for line in input:
-            action = U96Action(line)
+            dasBoot.ExecuteManeuver(Maneuver(line))
 
-            if(action.direction == "forward"):
-                forward += action.distance
-            else:
-                depth += (action.distance if action.direction == "down" else action.distance * -1)
-
-    return forward * depth;
+    return dasBoot.Position()
 
 def Part2():
-    forward = 0
-    depth = 0
-    aim = 0
+    dasBoot = U96Aimed()
 
     with open('./input.txt') as input:
         for line in input:
-            action = U96Action(line)
+            dasBoot.ExecuteManeuver(Maneuver(line))
 
-            if(action.direction == "forward"):
-                forward += action.distance
-                depth += action.distance * aim
-            else:
-                aim += (action.distance if action.direction == "down" else action.distance * -1)
-
-    return forward * depth;
+    return dasBoot.Position()
 
 print(f" Answer 1st part is {Part1()}")
 print(f" Answer 2st part is {Part2()}")
