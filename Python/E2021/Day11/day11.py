de1cd44735ced8c68a635cfcffad8c9fe4295078
Dimitry Slynko <slynko@gmail.com>
2021-12-13 21:27:55 +0100

    Day 11 final

--- Python/E2021/Day11/day11.py
+++ Python/E2021/Day11/day11.py
@@ -4,7 +4,7 @@ gridSize = -1
 octopuses = []
 
 def ReadInput():
-    with open("/Users/dimitry/Work/Study/AdventOfCode/2020/Python/E2021/Day11/input.test.txt") as input:
+    with open("./input.txt") as input:
         return input.read().splitlines()
 
 class Octopus:
@@ -13,6 +13,16 @@ class Octopus:
         self.col = col
         self.energy = energy
         self.isFlashed = False
+        self.neighbours = []
+
+    def FillNeibhours(self, octopuses):
+        gridSize = int(len(octopuses) ** 0.5) # sqrt
+        for (dRow, dCol) in [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]:
+            nRow = self.row + dRow
+            nCol = self.col + dCol
+
+            if(nRow >= 0 and nRow < gridSize and nCol >= 0 and nCol < gridSize):
+                self.neighbours.append(octopuses[nRow * gridSize + nCol])
 
     def FirstStep(self):
         self.energy += 1
@@ -23,13 +33,9 @@ class Octopus:
             self.KickNeibhours()
 
     def KickNeibhours(self):
-        for (dRow, dCol) in [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]:
-            nRow = self.row + dRow
-            nCol = self.col + dCol
-            if(nRow >= 0 and nRow < gridSize and nCol >= 0 and nCol < gridSize):
-                neighbour = GetOctopusAt(nRow, nCol)
-                neighbour.FirstStep()
-                neighbour.Flash()
+        for neighbour in self.neighbours:
+            neighbour.FirstStep()
+            neighbour.Flash()
 
     def FinalStep(self):
         flashes = 0
@@ -41,49 +47,21 @@ class Octopus:
 
         return flashes
 
-    def __repr__(self):
-        return self.__str__()
-    def __str__(self):
-        return f"{self.energy}@[{self.row}:{self.col}]"
+def PartX():
+    input = ReadInput()
 
-def GetOctopusAt(row, col) -> Octopus:
+    for row in range(len(input)):
+        for col in range(len(input[row])):
+            octopuses.append(Octopus(row, col, int(input[row][col])))
+
     for octopus in octopuses:
-        if(octopus.row == row and octopus.col == col):
-            return octopus
+        octopus.FillNeibhours(octopuses)
 
-def PrintOctopuses(octopuses: list[Octopus]):
-    print("---")
-    currLine = 0
-    line = ""
-    for octopus in octopuses:
-        if (currLine != octopus.row):
-            print(line)
-            line = ""
-            currLine += 1
-        line += f"{octopus.energy}"
-    print(line)
-
-def Part1(octopuses: list[Octopus]):
-    flashes = 0
-
-    for step in range(100):
-        for octopus in octopuses:
-            octopus.FirstStep()
-
-        for octopus in octopuses:
-            octopus.Flash()
-
-        for octopus in octopuses:
-            flashes += octopus.FinalStep()
-
-    return flashes
-
-def Part2(octopuses: list[Octopus]):
     step = 0
+    flashes = 0
 
     while(True):
         step += 1
-        #print(step)
 
         for octopus in octopuses:
             octopus.FirstStep()
@@ -91,21 +69,18 @@ def Part2(octopuses: list[Octopus]):
         for octopus in octopuses:
             octopus.Flash()
 
-        flashes = 0
+        stepFlashes = 0
 
         for octopus in octopuses:
-            flashes += octopus.FinalStep()
-        #PrintOctopuses(octopuses)
+            stepFlashes += octopus.FinalStep()
 
-        if(flashes == gridSize ** 2):
-            return step
+        flashes += stepFlashes
 
-input = ReadInput()
-gridSize = len(input)
+        if(step == 100):
+            print(f" Answer 1st part is {flashes}")
 
-for row in range(len(input)):
-    for col in range(len(input[row])):
-        octopuses.append(Octopus(row, col, int(input[row][col])))
+        if(stepFlashes == len(octopuses)):
+            print(f" Answer 2nd part is {step}")
+            break
 
-print(f" Answer 1st part is {Part1(octopuses)}")
-print(f" Answer 2st part is {Part2(octopuses)}")
+PartX()
